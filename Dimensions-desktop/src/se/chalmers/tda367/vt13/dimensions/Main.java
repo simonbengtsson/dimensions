6894d6963caf740a142229a54054b5fdaf7f1509
Kim Kling <kim.kling@hotmail.com>
2013-04-08 11:04:33 +0200

    Initial structure added, as well as Constant class

--- Dimensions-desktop/src/se/chalmers/tda367/vt13/dimensions/Main.java
+++ Dimensions-desktop/src/se/chalmers/tda367/vt13/dimensions/Main.java
@@ -1,12 +1,14 @@
 package se.chalmers.tda367.vt13.dimensions;
 
+import se.chalmers.tda367.vt13.dimensions.util.Constants;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "Dimensions";
+		cfg.title = Constants.NAME + " " + Constants.VERSION;
 		cfg.useGL20 = false;
 		cfg.width = 480;
 		cfg.height = 320;
