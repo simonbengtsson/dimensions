fa41674129c357d6c2d0388d5f02a8a738b5f4ff
Kim Kling <kim.kling@hotmail.com>
2013-04-09 12:40:55 +0200

    Added platform handeling in model

--- Dimensions/src/se/chalmers/tda367/vt13/dimensions/model/Model.java
+++ Dimensions/src/se/chalmers/tda367/vt13/dimensions/model/Model.java
@@ -1,17 +1,25 @@
 package se.chalmers.tda367.vt13.dimensions.model;
 
+import java.util.*;
+
 public class Model {
 	private float gravity = -9.82f;
 	private float worldSpeed = 10;
 	private Player player;
+	private List<Platform> platforms;
 	
 	
 	public Model(){
 		player = new NormalPlayer(this);
+		platforms = new ArrayList<Platform>();
 	}
 	
 	public float getGravity(){
 		return gravity;
+	}
+	
+	public void setGravity(float gravity){
+		this.gravity = gravity;
 	}
 	 
 	public void update(float time){
@@ -24,5 +32,17 @@ public class Model {
 	
 	public float getWorldSpeed(){
 		return worldSpeed;
+	}
+	
+	public void setWorldSpeed(float speed){
+		worldSpeed = speed;
+	}
+	
+	public void addPlatform(Platform p){
+		platforms.add(p);
+	}
+	
+	public void removePlatform(Platform p){
+		platforms.remove(p);
 	}
 }
