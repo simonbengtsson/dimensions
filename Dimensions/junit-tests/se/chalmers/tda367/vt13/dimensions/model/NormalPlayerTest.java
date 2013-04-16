ffff10484cca1e5fbe5d8da50a4ca29cb1b00bc9
Kim Kling <kim.kling@hotmail.com>
2013-04-16 18:05:00 +0200

    Fixed test

--- Dimensions/junit-tests/se/chalmers/tda367/vt13/dimensions/model/NormalPlayerTest.java
+++ Dimensions/junit-tests/se/chalmers/tda367/vt13/dimensions/model/NormalPlayerTest.java
@@ -4,17 +4,19 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import se.chalmers.tda367.vt13.dimensions.factories.ModelFactory;
+import se.chalmers.tda367.vt13.dimensions.model.levels.Level;
 
 public class NormalPlayerTest {
 
 	@Test
 	public void testOnPlatform() {
-		Model m = ModelFactory.getModel();
-		NormalPlayer np = new NormalPlayer(m);
-		np.onPlatform();
-		assertTrue(np.getVelocityY() == 0);
-		assertTrue(!np.isFalling());
+		Level lv = new Level("Level1");
+		Player player = new Player(new Vector3(10,100,0), new Vector3(50, 50, 0), new Vector3(2, 0, 0)
+			, 0.75f, 15f, true);
+		GameModel model = new GameModel(lv.getList(), player);
+		model.getPlayer().getIsGrounded();
+		assertTrue(model.getPlayer().getSpeed().getY() == 0);
+		assertTrue(model.getPlayer().getIsGrounded());
 	}
 
 }
