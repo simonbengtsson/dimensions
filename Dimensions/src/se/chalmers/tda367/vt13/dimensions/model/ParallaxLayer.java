b09897b5809bfcb8dd67341f1d8b3fd62536605b
egenvall <kim.egenvall@hotmail.com>
2013-05-07 21:19:27 +0200

    Background repeats, added 2 classes of parallax scrolling for future use

---
+++ Dimensions/src/se/chalmers/tda367/vt13/dimensions/model/ParallaxLayer.java
@@ -0,0 +1,29 @@
+package se.chalmers.tda367.vt13.dimensions.model;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class ParallaxLayer {
+
+	private TextureRegion region;
+	private float xRatio;
+	
+	public ParallaxLayer(TextureRegion region,float xRatio){
+		super();
+		this.region = region;
+		this.xRatio = xRatio;
+		
+	}
+	
+	public TextureRegion getRegion(){
+		return region;
+	}
+	
+	public float getxRatio(){
+		return xRatio;
+	}
+	
+	public void render(float xPos,float yPos, float width, float height, SpriteBatch sprite){
+		sprite.draw(region, xPos, yPos, width, height);
+	}
+}
