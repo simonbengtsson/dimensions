a3bf34a7762fb6fb78b877e7a47bf5b10ac36ec1
Simon Bengtsson <simonbengt@gmail.com>
2013-05-15 16:49:58 +0200

    Added listener to finish level button

--- Dimensions/src/se/chalmers/tda367/vt13/dimensions/controller/screens/LevelFinishedScreen.java
+++ Dimensions/src/se/chalmers/tda367/vt13/dimensions/controller/screens/LevelFinishedScreen.java
@@ -23,8 +23,10 @@ import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 public class LevelFinishedScreen extends AbstractMenuScreen {
 
+	private Dimensions game;
 	public LevelFinishedScreen(Dimensions game) {
 		super(game);
+		this.game = game;
 	}
 
 	Skin skin;
@@ -64,34 +66,22 @@ public class LevelFinishedScreen extends AbstractMenuScreen {
 		pixmap.fill();
 		skin.add("white", new Texture(pixmap));
 
-		// Generate a 1x1 white texture and store it in the skin named "white".
 		Pixmap pixmap2 = new Pixmap(Gdx.files.internal("data/play.png"));
 		skin.add("play", new Texture(pixmap2));
-		
-		
 
-		// Configure a TextButtonStyle and name it "default". Skin resources are stored by type, so this doesn't overwrite the font.
-		
-
-		// Create a table that fills the screen. Everything else will go inside this table.
 		Table table = new Table();
 		table.setFillParent(true);
 		stage.addActor(table);
 
-		// Create a button with the "default" TextButtonStyle. A 3rd parameter can be used to specify a name other than "default".
 		final MenuButton button = new MenuButton();
 		table.add(button);
 		
-		
-
-		// Add a listener to the button. ChangeListener is fired when the button's checked state changes, eg when clicked,
-		// Button#setChecked() is called, via a key press, etc. If the event.cancel() is called, the checked state will be reverted.
-		// ClickListener could have been used, but would only fire when clicked. Also, canceling a ClickListener event won't
 		// revert the checked state.
 		button.addListener(new ChangeListener() {
 			public void changed (ChangeEvent event, Actor actor) {
 				System.out.println(button.getStyle().pressedOffsetY);
 				System.out.println("Clicked!");
+				game.setScreen(game.getGameScreen());
 			}
 		});
 	}
