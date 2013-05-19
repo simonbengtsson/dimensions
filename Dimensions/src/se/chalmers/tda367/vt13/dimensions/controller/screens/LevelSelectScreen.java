bbd0da02182bb0c4d7f49f0470cf45d5d7224b0a
Kim Kling <kim.kling@hotmail.com>
2013-05-19 22:58:46 +0200

    Removed list of Levels in LevelHandler. Instead only using ProgressLevels.

--- Dimensions/src/se/chalmers/tda367/vt13/dimensions/controller/screens/LevelSelectScreen.java
+++ Dimensions/src/se/chalmers/tda367/vt13/dimensions/controller/screens/LevelSelectScreen.java
@@ -1,31 +1,35 @@
 package se.chalmers.tda367.vt13.dimensions.controller.screens;
 
-import java.util.List;
+import java.util.Collection;
 
 import se.chalmers.tda367.vt13.dimensions.controller.Dimensions;
-import se.chalmers.tda367.vt13.dimensions.model.Level;
 import se.chalmers.tda367.vt13.dimensions.model.LevelHandler;
+import se.chalmers.tda367.vt13.dimensions.model.progresshandler.ProgressLevel;
 
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class LevelSelectScreen extends AbstractMenuScreen {
-	
+
 	public LevelSelectScreen(final Dimensions game) {
 		super(game);
 		init();
 	}
 
 	private void init() {
-		List<Level> levels = LevelHandler.getInstance().getLevels();
+		Collection<ProgressLevel> levels = LevelHandler.getInstance().getProgressLevels();
 
-		for (final Level l : levels) {
-			TextButton levelButton = new TextButton(l.getName(),
+		for (final ProgressLevel pl : levels) {
+			String prefix = "";
+			if(pl.isCompleted()){
+				prefix = "Completed - ";
+			}
+			TextButton levelButton = new TextButton(prefix + pl.getLevel().getName(),
 					getButtonStyle());
 			levelButton.addListener(new ClickListener() {
 				public void clicked(InputEvent e, float x, float y) {
-					game.setScreen(new GameScreen(game, l));
+					game.setScreen(new GameScreen(game, pl.getLevel()));
 					dispose();
 				}
 			});
