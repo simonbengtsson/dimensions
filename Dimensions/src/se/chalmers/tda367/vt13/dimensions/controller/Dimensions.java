79f284eab6fb59f976f4d8103e8a7d4e3e1d8e52
Kim Kling <kim.kling@hotmail.com>
2013-05-19 16:52:11 +0200

    Got rid of LevelUtil

--- Dimensions/src/se/chalmers/tda367/vt13/dimensions/controller/Dimensions.java
+++ Dimensions/src/se/chalmers/tda367/vt13/dimensions/controller/Dimensions.java
@@ -1,7 +1,7 @@
 package se.chalmers.tda367.vt13.dimensions.controller;
 
 import se.chalmers.tda367.vt13.dimensions.controller.screens.SplashScreen;
-import se.chalmers.tda367.vt13.dimensions.util.LevelUtil;
+import se.chalmers.tda367.vt13.dimensions.model.LevelHandler;
 
 import com.badlogic.gdx.Game;
 
@@ -9,7 +9,7 @@ public class Dimensions extends Game {
 
 	@Override
 	public void create() {
-		LevelUtil.loadAllLevels(); // Loads all Levels into the game
+		LevelHandler.getInstance().load(); // Loads all Levels into the game
 		// Set startup screen
 		setScreen(new SplashScreen(this));
 	}
