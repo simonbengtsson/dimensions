f996130c5d48cc0cbc4599bfac39442c2f704c38
egenvall <kim.egenvall@hotmail.com>
2013-05-22 12:42:50 +0200

    Forgot updated playertest file

--- Dimensions/tests/se/chalmers/tda367/vt13/dimensions/model/PlayerTest.java
+++ Dimensions/tests/se/chalmers/tda367/vt13/dimensions/model/PlayerTest.java
@@ -1,9 +1,29 @@
 package se.chalmers.tda367.vt13.dimensions.model;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 
 
 public class PlayerTest {
 
+	
+	@Test
+	public void testEquals(){
+		Player p = new Player();
+		Player p2 = new Player();
+		Player p3 = new Player(new Vector3(10, 10, 10), new Vector3(2, 2, 2), new Vector3(0.5f,
+				0, 0), 1f, false);
+		
+		assertTrue(p!=p2);
+		assertTrue(p.equals(p2));
+		assertFalse(p.equals(p3));
+		
+		p2.setSpeed(new Vector3(0.5f,
+				0, 0));
+		assertFalse(p.equals(p2));
+	}
 //	@Test
 //	public void testJump() {
 //		Player p = new Player(new Vector3(10, 100, 0), new Vector3(50, 50, 0),
