6a5c672a70f207d8cd2864e5eb4b075f491e089f
Kim Kling <kim.kling@hotmail.com>
2013-05-22 16:30:25 +0200

    Extended usage of Assets.

--- Dimensions/src/se/chalmers/tda367/vt13/dimensions/controller/Dimensions.java
+++ Dimensions/src/se/chalmers/tda367/vt13/dimensions/controller/Dimensions.java
@@ -2,10 +2,10 @@ package se.chalmers.tda367.vt13.dimensions.controller;
 
 import se.chalmers.tda367.vt13.dimensions.controller.screens.SplashScreen;
 import se.chalmers.tda367.vt13.dimensions.model.LevelHandler;
+import se.chalmers.tda367.vt13.dimensions.util.Assets;
 import se.chalmers.tda367.vt13.dimensions.util.Storage;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
 
 public class Dimensions extends Game {
 
@@ -14,6 +14,7 @@ public class Dimensions extends Game {
 		if(!Storage.loadProgress()){
 			LevelHandler.getInstance().load(); // Loads all new Levels into the game
 		}
+		Assets.loadAllAudioVisual();
 		
 		// Set startup screen
 		setScreen(new SplashScreen(this));
