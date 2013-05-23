23d4663c9add319f4676681f9f76a15be87c6fe8
kim egenvall <kim.egenvall@hotmail.com>
2013-05-23 16:19:12 +0200

    Started building a new level,mega difficult to pass the level so far, added some tiles

--- Dimensions/src/se/chalmers/tda367/vt13/dimensions/model/LevelHandler.java
+++ Dimensions/src/se/chalmers/tda367/vt13/dimensions/model/LevelHandler.java
@@ -148,5 +148,13 @@ public class LevelHandler {
 		Level natureLevel = new Level("Nature", natureList,
 				"tiled_maps/natureXY.tmx", "tiled_maps/natureXZ.tmx", 205);
 		registerLevel(natureLevel);
+		
+		// crazy Level
+				List<GameObject> crazyList = new ArrayList<GameObject>();
+				natureList.add(new DimensionChangePowerUp(new Vector3(60, 15,
+						10), new Vector3(1, 1, 1), new Vector3()));
+				Level crazyLevel = new Level("Crazy", crazyList,
+						"tiled_maps/crazyXY.tmx", "tiled_maps/crazyXZ.tmx", 205);
+				registerLevel(crazyLevel);
 	}
 }
