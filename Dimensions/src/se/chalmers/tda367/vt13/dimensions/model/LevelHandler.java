b19dc6dc765b1538b5ad7927a36008ae1e1ec927
Simon Bengtsson <simonbengt@gmail.com>
2013-05-26 23:50:28 +0200

    Small final fixes

--- Dimensions/src/se/chalmers/tda367/vt13/dimensions/model/LevelHandler.java
+++ Dimensions/src/se/chalmers/tda367/vt13/dimensions/model/LevelHandler.java
@@ -10,7 +10,6 @@ import java.util.List;
 import se.chalmers.tda367.vt13.dimensions.model.powerup.DimensionChangePowerUp;
 import se.chalmers.tda367.vt13.dimensions.model.powerup.LowGravityPowerUp;
 import se.chalmers.tda367.vt13.dimensions.model.powerup.SpeedPowerUp;
-import se.chalmers.tda367.vt13.dimensions.model.progresshandler.ProgressLevel;
 
 public class LevelHandler {
 	private Deque<ProgressLevel> progressLevels;
