11311d7f6237c165d0f0e89f05fe54143228991c
Simon Bengtsson <simonbengt@gmail.com>
2013-05-26 22:07:15 +0200

    Removed unnecessary files

--- Dimensions/src/se/chalmers/tda367/vt13/dimensions/controller/Dimensions.java
+++ Dimensions/src/se/chalmers/tda367/vt13/dimensions/controller/Dimensions.java
@@ -1,6 +1,6 @@
 package se.chalmers.tda367.vt13.dimensions.controller;
 
-import se.chalmers.tda367.vt13.dimensions.controller.screens.SplashScreen;
+import se.chalmers.tda367.vt13.dimensions.controller.screens.MainMenuScreen;
 import se.chalmers.tda367.vt13.dimensions.model.LevelHandler;
 import se.chalmers.tda367.vt13.dimensions.util.Assets;
 import se.chalmers.tda367.vt13.dimensions.util.Storage;
@@ -12,12 +12,9 @@ public class Dimensions extends Game {
 	@Override
 	public void create() {
 		if (!Storage.loadProgress()) {
-			LevelHandler.getInstance().load(); // Loads all new Levels into the
-												// game
+			LevelHandler.getInstance().load();
 		}
 		Assets.loadAllAudioVisual();
-
-		// Set startup screen
-		setScreen(new SplashScreen(this));
+		setScreen(new MainMenuScreen(this));
 	}
 }
--- Dimensions/src/se/chalmers/tda367/vt13/dimensions/model/ModelInterface.java
+++
@@ -1,7 +0,0 @@
-package se.chalmers.tda367.vt13.dimensions.model;
-
-public interface ModelInterface {
-	public void playerJump();
-
-	public void setGravity();
-}
